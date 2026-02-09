0eebbf8100ee55d1041335643f31434dde4f2dfe
EmmanuelPuicon <emmanuelpuicon11@gmail.com>
2026-02-08 21:53:32 -0500

    Login v1.0

--- app/modules/users/router.py
+++ app/modules/users/router.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.db.database import get_db
 from . import models, schemas
 from app.core.util.password import get_password_hash
+from app.core.util.password import verify_password
 
 router = APIRouter(prefix="/usuarios", tags=["Usuarios"])
 
@@ -18,3 +19,33 @@ def crear_usuario(usuario: schemas.UsuarioCreate, db: Session = Depends(get_db))
     db.commit()
     db.refresh(db_user)
     return db_user
+
+@router.post("/login", response_model=schemas.LoginResponse)
+def login(credentials: schemas.UsuarioLogin, db: Session = Depends(get_db)):
+    # 1. Buscar al usuario por nombre de usuario
+    user = db.query(models.Usuario).filter(models.Usuario.username == credentials.username).first()
+    
+    # 2. Validar existencia
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Credenciales inválidas"
+        )
+
+    # 3. Validar contraseña usando tu función de bcrypt
+    if not verify_password(credentials.password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Credenciales inválidas"
+        )
+    
+    # 4. Validar si el usuario está activo
+    if not user.activo:
+        raise HTTPException(status_code=403, detail="Cuenta desactivada")
+
+    # 5. Retornar los datos que el frontend necesita para el UseContext(aqui se usara JWT)
+    return {
+        "username": user.username,
+        "rol": user.rol,
+        "status": "success"
+    }
--- app/modules/users/schemas.py
+++ app/modules/users/schemas.py
@@ -14,3 +14,15 @@ class UsuarioResponse(UsuarioBase):
     id_usuario: int
     fecha_creacion: datetime
     model_config = ConfigDict(from_attributes=True)
+
+# Para recibir los datos del formulario de login
+class UsuarioLogin(BaseModel):
+    username: str
+    password: str
+
+# Para enviar la respuesta tras un login exitoso
+class LoginResponse(BaseModel):
+    username: str
+    rol: str
+    status: str = "success"
+    # Aquí podrías añadir un access_token más adelante
